d0bf2275a77b3a2a0cf012393909bb8db2505138
boram065 <w2216@e-mirim.hs.kr>
2024-01-06 22:43:21 +0900

    경로 변경

---
+++ app/src/main/java/com/example/appjam/WorryActivity.kt
@@ -0,0 +1,23 @@
+package com.example.appjam
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class WorryActivity : AppCompatActivity() {
+
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worry)
+
+        val imageView: ImageView = findViewById(R.id.profile)
+        imageView.setImageResource(R.drawable.baseline_person_24)
+    }
+}
