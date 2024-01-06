25b3fa5e9372f4da6d13d85eb7702c991b2952a4
Github <s23065@gsm.hs.kr>
2024-01-06 21:29:19 +0900

    :sparkles: :: Splash Screen & Login Screen publishing

--- app/src/main/java/com/example/appjam/MainActivity.kt
+++ app/src/main/java/com/example/appjam/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appjam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
 import com.example.appjam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val etPassword = binding.passwordedittext
+        val btnHide = binding.btnPasswordHide
+
+        btnHide.setOnClickListener {
+            when (btnHide.tag as String) {
+                "0" -> {
+                    btnHide.tag = "1"
+                    etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    btnHide.setImageResource(R.drawable.ic_show)
+                }
+
+                "1" -> {
+                    btnHide.tag = "0"
+                    etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    btnHide.setImageResource(R.drawable.ic_show)
+                }
+            }
+            etPassword.setSelection(etPassword.text!!.length)
+        }
     }
 }
