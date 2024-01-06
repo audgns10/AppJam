f8424deccf132020f4e438e6526e9eff055f6c37
boram065 <w2216@e-mirim.hs.kr>
2024-01-07 02:45:36 +0900

    화면 이동 구현

---
+++ app/src/main/java/com/example/appjam/MyComfort2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.appjam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyComfort2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_comfort2)
+    }
+}
